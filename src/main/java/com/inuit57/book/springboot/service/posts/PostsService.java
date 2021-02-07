03f2dcea7ee235ecc65ebbb5c1f2945e6d753b16
narD <inuit57@naver.com>
2021-02-07 12:28:32 +0900

    DB update 문까지 학습

--- src/main/java/com/inuit57/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/inuit57/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.inuit57.book.springboot.service.posts;
 
+import com.inuit57.book.springboot.domain.posts.Posts;
 import com.inuit57.book.springboot.domain.posts.PostsRepository;
+import com.inuit57.book.springboot.web.dto.PostsResponseDto;
 import com.inuit57.book.springboot.web.dto.PostsSaveRequestDto;
+import com.inuit57.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +20,21 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow( () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
 
 }
